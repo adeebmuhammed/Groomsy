5b6e63785d7e938c3dda814dab5d0d686496e306
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-07-25 07:51:02 +0530

    commenced slot management for barber

---
+++ backend/src/models/slots.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface ISlot extends Document {
+  startTime: Date;
+  endTime: Date;
+  price: number;
+  date: Date;
+  barberId: mongoose.Types.ObjectId;
+}
+
+const SlotSchema: Schema = new Schema({
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
+  price: { type: Number, required: true },
+  date: { type: Date, required: true },
+  barberId: { type: mongoose.Types.ObjectId, ref: 'Barber', required: true }
+},{ timestamps: true });
+
+const Slots = mongoose.model<ISlot>('Slot', SlotSchema);
+export default Slots;
