796c6a7f49c5efcdb115b64afd34e919806c891a
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-08-25 22:45:46 +0530

    removed calling get api after block/unblock any entities from admin side

--- frontend/src/app/pages/admin/admin-barbers-list/admin-barbers-list.component.ts
+++ frontend/src/app/pages/admin/admin-barbers-list/admin-barbers-list.component.ts
@@ -10,11 +10,16 @@ import { IBarber } from '../../../interfaces/interfaces';
 
 @Component({
   selector: 'app-admin-barbers-list',
-  imports: [ AdminHeaderComponent,AdminFooterComponent,AdminSidebarComponent,CommonModule,AdminTableComponent ],
+  imports: [
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminSidebarComponent,
+    CommonModule,
+    AdminTableComponent,
+  ],
   templateUrl: './admin-barbers-list.component.html',
-  styleUrl: './admin-barbers-list.component.css'
+  styleUrl: './admin-barbers-list.component.css',
 })
-
 export class AdminBarbersListComponent implements OnInit {
   barbers: IBarber[] = [];
   currentPage = 1;
@@ -28,17 +33,18 @@ export class AdminBarbersListComponent implements OnInit {
     { key: 'phone', label: 'Phone' },
     { key: 'district', label: 'District' },
     { key: 'status', label: 'Status', isStatus: true },
-    { key: 'createdAt', label: 'Created', isDate: true }
+    { key: 'createdAt', label: 'Created', isDate: true },
   ];
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
-    this.fetchBarbers()
+    this.fetchBarbers();
   }
 
   fetchBarbers(): void {
-    this.adminService.listBarbers(this.searchTerm, this.currentPage, this.itemsPerPage)
+    this.adminService
+      .listBarbers(this.searchTerm, this.currentPage, this.itemsPerPage)
       .subscribe((res) => {
         this.barbers = res?.data || [];
         this.totalPages = res?.pagination?.totalPages || 1;
@@ -57,28 +63,31 @@ export class AdminBarbersListComponent implements OnInit {
   }
 
   updateBarberStatus(barber: IBarber): void {
-        const status = barber.status;
-        
-        this.adminService.updateBarberStatus(barber.id, status).subscribe({
-          next: (res) => {
-            Swal.fire({
-                        icon: 'success',
-                        title: 'Status Updated',
-                        text: res.message || 'Barber Status Updation Successful',
-                        timer: 2000,
-                        showConfirmButton: false,
-                      }).then(()=>{
-                        this.fetchBarbers()
-                      })
-          },
-          error: (err) => {
-            Swal.fire({
-                        icon: 'error',
-                        title: 'Status Updation Failed',
-                        text: 'Barber Status Updation Failed',
-                      });
-            console.error('Error updating barber status:', err);
-          }
+    const status = barber.status;
+
+    this.adminService.updateBarberStatus(barber.id, status).subscribe({
+      next: (res) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Status Updated',
+          text: res.message || 'Barber Status Updation Successful',
+          timer: 2000,
+          showConfirmButton: false,
+        }).then(() => {
+          const newStatus = status === 'blocked' ? 'active' : 'blocked';
+          this.barbers = this.barbers.map((b) =>
+            b.id === barber.id ? { ...b, status: newStatus } : b
+          );
         });
-      }
+      },
+      error: (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Status Updation Failed',
+          text: 'Barber Status Updation Failed',
+        });
+        console.error('Error updating barber status:', err);
+      },
+    });
+  }
 }
