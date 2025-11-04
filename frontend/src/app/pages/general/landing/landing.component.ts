f0750fef85d0292bc39e722ca260bae0842b2e05
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-11-04 22:42:44 +0530

    added a role check when initializing landing component

--- frontend/src/app/pages/general/landing/landing.component.ts
+++ frontend/src/app/pages/general/landing/landing.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ROLES } from '../../../constants/roles';
+import { USER_ROUTES_PATHS } from '../../../constants/user-route.constant';
+import { BARBER_ROUTES_PATHS } from '../../../constants/barber-route.constant';
+import { ADMIN_ROUTES_PATHS } from '../../../constants/admin-route.constant';
 
 @Component({
   selector: 'app-landing',
@@ -7,6 +11,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
   currentYear = new Date().getFullYear();
+
+  private router = inject(Router)
+
+  ngOnInit(): void {
+    const role = localStorage.getItem('role')
+
+    if (role && role === ROLES.USER) {
+      this.router.navigate([USER_ROUTES_PATHS.SIGNIN])
+    }else if (role && role === ROLES.BARBER) {
+      this.router.navigate([BARBER_ROUTES_PATHS.SIGNIN])
+    }else if (role && role === ROLES.ADMIN) {
+      this.router.navigate([ADMIN_ROUTES_PATHS.SIGNIN])
+    }else {
+      this.router.navigate(["/"])
+    }
+  }
 }
