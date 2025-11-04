52b6f08897ef94fc34adeae0667fc632453ae70c
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-11-04 22:36:03 +0530

    added landing page and role based navigation while using pwa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { USER_ROUTES } from './routes/user.routes';
 import { BARBER_ROUTES } from './routes/barber.routes';
 import { ADMIN_ROUTES } from './routes/admin.routes';
+import { LandingComponent } from './pages/general/landing/landing.component';
 
 export const routes: Routes = [
   ...USER_ROUTES,
   ...ADMIN_ROUTES,
   ...BARBER_ROUTES,
+  {
+      path: "",
+      component: LandingComponent
+    }
 ];
