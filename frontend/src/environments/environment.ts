edaca68f8e2fe31b971e6948122bcd2bdbe34a74
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-11-06 06:21:54 +0530

    created new s3 bucket

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:5000/api',
-  s3Bucket: 'groomsy-profile-pictures',
-  awsRegion: 'us-east-1'
+  s3Bucket: 'groomsy-profile-picture',
+  awsRegion: 'eu-north-1'
 };
