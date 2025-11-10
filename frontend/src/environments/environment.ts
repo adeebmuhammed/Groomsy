1d4d97dc2b71acfed9f45205fc230b885e42ee90
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-11-10 09:29:59 +0530

    chnaged api

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5000/api',
+  apiBaseUrl: 'https://groomsy.shop/api',
   s3Bucket: 'groomsy-profile-picture',
   awsRegion: 'eu-north-1'
 };
